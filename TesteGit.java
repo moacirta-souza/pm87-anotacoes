610b3151c878762b9227d49b124572714a661b2b
moacir <mujolo@gmail.com>
2016-09-13 10:44:53 -0300

    Mais mensagens

--- TesteGit.java
+++ TesteGit.java
@@ -2,5 +2,7 @@ public class TesteGit {
 	public static void main(String args[]) {
 		System.out.println("Exemplo de teste do uso do git");
 		System.out.println("Exemplo2 de teste do uso do git");
+		System.out.println("Trabalhando com branchs");
+
 	}
 }
